49a65298d432645c98468481797f06412e34d840
LeonardoTalanski <leonardo.e.t@hotmail.com>
2023-07-07 09:52:09 -0300

    aplicação de pasta assets

---
+++ assets/scripts/estilos.js
@@ -0,0 +1,7 @@
+const styles = document.createElement('link');
+
+styles.setAttribute('rel', 'stylesheet');
+
+styles.setAttribute('href', './assets/styles/style.css');
+
+document.head.appendChild(styles);
